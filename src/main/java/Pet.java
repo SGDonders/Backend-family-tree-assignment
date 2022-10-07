46e3bb9ac49792f5c92374a3d2d1e6562b04be39
SGDonders <donders_sg@live.com>
2022-10-07 14:48:03 +0200

    update classes

---
+++ src/main/java/Pet.java
@@ -0,0 +1,12 @@
+public class Pet {
+    public String name;
+    public int age;
+    public String species;
+    public String owner;
+
+    public Pet(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+}
