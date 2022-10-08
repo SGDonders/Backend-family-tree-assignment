ae06524bd1bd4d9219fa5ec8779780a844394817
SGDonders <donders_sg@live.com>
2022-10-08 16:34:34 +0200

    Made changes to instance variables

--- src/main/java/Pet.java
+++ src/main/java/Pet.java
@@ -1,12 +1,47 @@
 public class Pet {
+
+    /* instance variables */
+
     public String name;
     public int age;
     public String species;
     public String owner;
 
+    /* constructor */
+
     public Pet(String name, int age, String species) {
         this.name = name;
         this.age = age;
         this.species = species;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    /* methods */
+
+    public void addOwner() {
+
     }
 }
